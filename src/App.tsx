c1f283d1bfd10967a385939adf0f82d3e1ce1873
yaga49 <komarov.vova2001@mail.ru>
2023-01-09 10:20:41 +0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Rating/>
+      <Accordion/>
     </div>
   );
 }
+
+function Accordion(){
+  return(
+      <div>
+        <h3>menu</h3>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+  )
+}
+
+function Rating(){
+  return(
+      <div>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+      </div>
+  )
+}
+
+function Star(){
+    return(
+        <div>star</div>
+    )
+}
+
 
 export default App;
